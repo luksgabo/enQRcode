6757d32518e051fb6fb4e01a2645c868f585cbcb
Lucas local <lg.borges12@gmail.com>
2026-06-07 19:34:48 +0200

    changed Grid type to grid

--- makesquare.cpp
+++ makesquare.cpp
@@ -2,19 +2,19 @@
 #include <vector>
 
 typedef std::vector<bool> vec;
-typedef std::vector<std::vector<bool>> Grid;
+typedef std::vector<std::vector<bool>> grid;
 
 const int length {25}; //version 2 type (25x25)
 char white_symbol= '#', black_symbol =' ';
-Grid Board;
+grid Board;
 
-void write_on_board(Grid& Board, int length);
+void write_on_board(grid& Board, int length);
 void print_line(int length, char symbol );
-Grid square_module(int length);
-void past_grid(Grid& LargeGrid, const Grid& SmallGrid,
+grid square_module(int length);
+void past_grid(grid& LargeGrid, const grid& SmallGrid,
                 size_t start_row, size_t start_col);
 
-void print_board(const Grid& Board);
+void print_board(const grid& Board);
 
 int main(void)
 {
@@ -23,7 +23,7 @@ int main(void)
     print_board(Board);
 }
 
-void write_on_board(Grid& Board, int length){
+void write_on_board(grid& Board, int length){
     Board.resize(length, std::vector<bool>(length));
     for (int i{0}; i<length; ++i){
         for (int j{0}; j<length; ++j){
@@ -31,19 +31,19 @@ void write_on_board(Grid& Board, int length){
         };
     };
     // square modules in corners
-    Grid square {square_module(7)};
+    grid square {square_module(7)};
     past_grid(Board, square, 0, 0);
     past_grid(Board, square, 0, length-7);
     past_grid(Board, square, length-7, 0);
-    Grid align_square {square_module(5)};
+    grid align_square {square_module(5)};
     past_grid(Board, align_square, 16, 16);
 
     // timing pattern
 
 }
 
-Grid square_module(int square_length) {
-    Grid square{};
+grid square_module(int square_length) {
+    grid square{};
 
     square.resize(square_length,std::vector<bool>(square_length));
     square[0].flip();
@@ -73,7 +73,7 @@ Grid square_module(int square_length) {
     return square;
 }
 
-void past_grid(Grid& LargeGrid, const Grid& SmallGrid,
+void past_grid(grid& LargeGrid, const grid& SmallGrid,
                 size_t start_row, size_t start_col){
     size_t small_lenth{SmallGrid.size()};
     for (int i{0};i < small_lenth; ++i){
@@ -88,7 +88,7 @@ void print_line(int box_length, char symbol = white_symbol) {
     std::cout << symbol << std::endl;
 }
 
-void print_board(const Grid& Board){
+void print_board(const grid& Board){
     size_t box_length { Board[0].size()};
 
     // std::cout << std::string(box_length,'x') << '\n';
