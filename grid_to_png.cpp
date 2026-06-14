bd559785fd9cc8f447fe447f46fae3e4aad5b95c
Lucas local <lg.borges12@gmail.com>
2026-06-14 13:12:40 +0200

    fixed color in png

--- grid_to_png.cpp
+++ grid_to_png.cpp
@@ -17,7 +17,7 @@ void writeMatrixToPng(
     size_t grid_width{matrix[0].size() * scale};
 
     std::vector<uint8_t> pixels(grid_width * grid_heigth * 3);
-    // single row vector 
+    // single row vector to allocate pixels buffer
 
     for (uint32_t y = 0; y < grid_heigth; ++y) {
         for (uint32_t x = 0; x < grid_width; ++x) {
@@ -26,11 +26,11 @@ void writeMatrixToPng(
             uint32_t sy = y / scale;
 
             uint8_t bit = matrix[sx][sy];
-                // static_cast<size_t>(sy) * grid_width + sx];
-            uint8_t value = bit ? 255 : 0 ; // 0 -> black, 1 -> white
+            uint8_t value = bit ? 0 : 255 ; // 1 -> black, 0 -> white
 
             size_t idx = (static_cast<size_t>(y) * grid_width + x) * 3;
 
+            // Add color options
             pixels[idx + 0] = value;
             pixels[idx + 1] = value;
             pixels[idx + 2] = value;
